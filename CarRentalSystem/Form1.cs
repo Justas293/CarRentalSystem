8a964f5be17e73d9ce08b9fad7d4aa0d18b4184d
Justas <justasrasl@yahoo.com>
2016-12-02 13:32:36 +0200

    Initial commit

--- CarRentalSystem/Form1.cs
+++ CarRentalSystem/Form1.cs
@@ -16,5 +16,10 @@ namespace CarRentalSystem
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
